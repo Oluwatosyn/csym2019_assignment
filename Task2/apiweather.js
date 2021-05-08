fa3dee8f8edee5940c83dc76fbb908f9b88e3d20
Oluwatosyn <tosinolubanjo77@gmail.com>
2021-05-08 20:52:04 +0100

    2nd commit

--- Task2/apiweather.js
+++ Task2/apiweather.js
@@ -1,221 +1,90 @@
-let apiKey = 'ce7b672af0382b6a9b6c535ca2be9a81'
+let apiKey = 'ce7b672af0382b6a9b6c535ca2be9a81';
 let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
-let englandInfo = "";
-let nirelandInfo ="";
-let scotlandInfo ="";
-let walesInfo = "";
-let txt = "";
 
 $(document).ready(function() {
-    $("#england-cities").load("england-cities.html"); 
-    $("#england-cities").change(function() {
-        
-    englandInfo = $(this).val();
-    apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=' + englandInfo +  '&appid=' + apiKey ;  
-        
-		ajaxLoad();
+	$("#countries").change(function() {
+		let country = $(this).val();
 
-        
-			
- });	
-});
+		if (country == "england") {
+			$("#cities").load("england-cities.html");
 
-$(document).ready(function() {
-    $("#nireland-cities").load("nireland-cities.html"); 
-    $("#nireland-cities").change(function() {
-        
-        nirelandInfo = $(this).val();
-    apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=' + nirelandInfo +  '&appid=' + apiKey ;  
-        
-		ajaxLoad();
+		}
+		if (country == "nireland") {
+			$("#cities").load("nireland-cities.html");
+		}
 
-        
-			
- });	
-});
+		if (country == "scotland") {
+			$("#cities").load("scotland-cities.html");
+		}
+		if (country == "wales") {
+			$("#cities").load("wales-cities.html");
+		}
+		$("#cities").change(function() {
+			let city = $(this).val();
+			apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+			$.ajax({
+				url: apiUrl,
+				type: "GET",
+				dataType: "json",
+				success: function(response) {
+					console.log(response);
+					let txt = "";
+					// let info = "";
 
-$(document).ready(function() {
-    $("#scotland-cities").load("scotland-cities.html"); 
-    $("#scotland-cities").change(function() {
-        
-        scotlandInfo = $(this).val();
-    apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=' + scotlandInfo +  '&appid=' + apiKey ;  
-        
-		ajaxLoad();
+					txt += "<table><tr>";
+					txt += "<th>City Name</th>";
+					txt += "<th>Date</th>";
+					txt += "<th>Weather Conditions</th>";
+					txt += "<th>Temperature</th>";
+					txt += "<th>Wind Speed</th>";
+					txt += "<th>Wind Direction</th>";
+					txt += "<th>Weather Icon</th>";
+					txt += "</tr>";
 
-        
-			
- });	
-});
+					txt += "<tr>";
 
-$(document).ready(function() {
-    $("#wales-cities").load("wales-cities.html"); 
-    $("#wales-cities").change(function() {
-        
-        walesInfo = $(this).val();
-    apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=' + walesInfo +  '&appid=' + apiKey ;  
-        
-		ajaxLoad();
+					txt += "<td>" + response.name + "</td>";
+					txt += "<td>" + response.date + "</td>";
+					txt += "<td>" + response.weather[0].description + "</td>";
+					txt += "<td>" + response.main.temp + "</td>";
+					txt += "<td>" + response.wind.speed + "</td>";
+					txt += "<td>" + response.wind.dir + "</td>";
 
-        
-			
- });	
-});
 
-function ajaxLoad(){
-$.ajax( {
-           url: apiUrl,
-           type: "GET",
-           dataType: "json", 
-           success: function(response){
-               console.log(response);
-               let txt = "";
+					$.each(response.weather, function() {
 
-               txt += "<table><tr>"
-           txt += "<th>City Name</th>"
-           txt += "<th>Date</th>"
-           txt += "<th>Weather Conditions</th>"
-           txt += "<th>Temperature</th>"
-           txt += "<th>Wind Speed</th>"
-           txt += "<th>Wind Direction</th>"
-           txt += "<th>Weather Icon</th>"
-           txt += "</tr>";
+						let iconID = response.weather[0].icon;
+						let iconPath = "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
+						//console.log(iconPath);
 
-           txt += "<tr>"
-					
-			txt += "<td>" + response.name + "</td>"
-            txt += "<td>" + response.date+ "</td>"
-            txt += "<td>" + response.weather[0].description + "</td>"
-			txt += "<td>" + response.main.temp + "</td>"
-			txt += "<td>" + response.wind.speed + "</td>"
-			txt += "<td>" + response.wind.dir + "</td>"
-		
+						txt += "<td><img src=\"" + iconPath + "\"alt=\"" + response.weather[0].description + "\"></td>";
 
-            $.each(response.weather, function() {  
-        
-             let iconID = response.weather[0].icon 
-             let iconPath  = "http://openweathermap.org/img/wn/"+ iconID +"@2x.png"
-             //console.log(iconPath);
+						txt += "</tr></table>";
+					});
 
-             txt += "<td><img src=\""+ iconPath + "\"alt=\""+response.weather[0].description+"\"></td>"
-			
-			txt += "</tr></table>"
-            });
+					$.each(response, function() {
+						$("#citiesinfo").html("");
+						$("#citiesinfo").append(txt);
+						if ($("#citiesinfo").attr("hidden")) {
+							$("#citiesinfo").show();
+						}
 
-            $("#englandinfo").html("");					
-			$("#englandinfo").append(txt);
-			if ($("#englandinfo").attr("hidden")) {
-				$("#englandinfo").show();	
-			}
-			
-			$("#englandinfo").css(
-			{	
-				"border-color": "#C1E0FF", 
-				"border-weight":"1px", 
-				"border-style":"solid",
-				"margin-top":"1rem",
-				"width":"50%",
-				"padding":"0.5rem"
+						$("#citiesinfo").css({
+							"border-color": "#C1E0FF",
+							"border-weight": "1px",
+							"border-style": "solid",
+							"margin-top": "1rem",
+							"width": "50%",
+							"padding": "0.5rem"
+						});
+					});
+
+				},
+
+				error: function(xhr, error) {
+					$("#info").append(error.toUpperCase() + ". HTTP status: " + xhr.status);
+				}
 			});
-
-
-            $.each(response.weather, function() {  
-        
-                let iconID = response.weather[0].icon 
-                let iconPath  = "http://openweathermap.org/img/wn/"+ iconID +"@2x.png"
-                //console.log(iconPath);
-            
-                txt += "<td><img src=\""+ iconPath + "\"alt=\""+response.weather[0].description+"\"></td>"
-               
-               txt += "</tr></table>"
-               });
-            
-               $("#nirelandInfo").html("");					
-               $("#nirelandInfo").append(txt);
-               if ($("#nirelandInfo").attr("hidden")) {
-                   $("#enirelandInfo").show();	
-               }
-               
-               $("#nirelandInfo").css(
-               {	
-                   "border-color": "#C1E0FF", 
-                   "border-weight":"1px", 
-                   "border-style":"solid",
-                   "margin-top":"1rem",
-                   "width":"50%",
-                   "padding":"0.5rem"
-               });
-
-
-               $.each(response.weather, function() {  
-        
-                let iconID = response.weather[0].icon 
-                let iconPath  = "http://openweathermap.org/img/wn/"+ iconID +"@2x.png"
-                //console.log(iconPath);
-            
-                txt += "<td><img src=\""+ iconPath + "\"alt=\""+response.weather[0].description+"\"></td>"
-               
-               txt += "</tr></table>"
-               });
-            
-               $("#scotlandInfo").html("");					
-               $("#scotlandInfo").append(txt);
-               if ($("#scotlandInfo").attr("hidden")) {
-                   $("#scotlandInfo").show();	
-               }
-               
-               $("#scotlandInfo").css(
-               {	
-                   "border-color": "#C1E0FF", 
-                   "border-weight":"1px", 
-                   "border-style":"solid",
-                   "margin-top":"1rem",
-                   "width":"50%",
-                   "padding":"0.5rem"
-               });
-
-               $.each(response.weather, function() {  
-        
-                let iconID = response.weather[0].icon 
-                let iconPath  = "http://openweathermap.org/img/wn/"+ iconID +"@2x.png"
-                //console.log(iconPath);
-            
-                txt += "<td><img src=\""+ iconPath + "\"alt=\""+response.weather[0].description+"\"></td>"
-               
-               txt += "</tr></table>"
-               });
-            
-               $("#walesInfo").html("");					
-               $("#walesInfo").append(txt);
-               if ($("#walesInfo").attr("hidden")) {
-                   $("#walesInfo").show();	
-               }
-               
-               $("#walesInfo").css(
-               {	
-                   "border-color": "#C1E0FF", 
-                   "border-weight":"1px", 
-                   "border-style":"solid",
-                   "margin-top":"1rem",
-                   "width":"50%",
-                   "padding":"0.5rem"
-               });
-
-
-
-
-               
-
-
-
-          
-			
-			
-			
-           },	
-
-           error: function(xhr, error, message){
-            //   $("#info").append(error.toUpperCase() + ". HTTP status: " + xhr.status);
-           }
-       });    
-	 }
+		});
+	});
+});
